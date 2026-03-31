e96951197b5c1211eb8efcbb00243bf334c620c0
Azizur-Rahman-Emon <azizurrahmanemoncs13@gmail.com>
2026-03-31 23:18:55 +0600

    Create DFS Traversal.cpp

---
+++ Depth First Search/DFS Traversal.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int>adj_list[1008];
+bool vis[1008];
+void dfs(int src)
+{
+    cout<<src<<" ";
+    vis[src]=true;
+    for(int child:adj_list[src])
+    {
+        if(!vis[child])
+        dfs(child);
+    }
+}
+int main()
+{
+    int n,e;
+    cin>>n>>e;
+    while(n--)
+    {
+        int a,b;
+        cin>>a>>b;
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a);
+    }
+    memset(vis,false,sizeof(vis));
+    dfs(0);
+}
