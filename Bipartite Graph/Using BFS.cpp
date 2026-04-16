7d548183700a377598cf0c0334f3a1abefcae32c
Azizur-Rahman-Emon <azizurrahmanemoncs13@gmail.com>
2026-04-16 19:50:10 +0600

    Update Using BFS.cpp

--- Bipartite Graph/Using BFS.cpp
+++ Bipartite Graph/Using BFS.cpp
@@ -1,58 +1,95 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int N = 2e5 + 5;
-vector<int> adj[N];
-int color[N]; // -1 = not visited, 0/1 = two colors
+const int mx = 2e5 + 5;
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
+vector<int> adj[mx];
+vector<int> colour(mx);
 
+bool bfs(int src, int &cnt0, int &cnt1)
+{
+    queue<int> q;
+    q.push(src);
+    colour[src] = 0;
+    cnt0++;
+
+    bool ok = true;
+
+    while (!q.empty())
+    {
+        int u = q.front();
+        q.pop();
+
+        for (auto v : adj[u])
+        {
+            if (colour[v] == -1)
+            {
+                colour[v] = colour[u] ^ 1;
+
+                if (colour[v] == 0) cnt0++;
+                else cnt1++;
+
+                q.push(v);
+            }
+            else if (colour[v] == colour[u])
+            {
+                ok = false;
+            }
+        }
+    }
+
+    return ok;
+}
+
+void dosomethingbig()
+{
     int n, m;
     cin >> n >> m;
 
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1; i <= n; i++)
+    {
         adj[i].clear();
-        color[i] = -1;
+        colour[i] = -1;
     }
 
-    for (int i = 0; i < m; i++) {
+    for (int i = 0; i < m; i++)
+    {
         int u, v;
         cin >> u >> v;
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
 
-    int answer = 0;
+    int ans = 0;
 
-    for (int i = 1; i <= n; i++) {
-        if (color[i] != -1) continue;
+    for (int i = 1; i <= n; i++)
+    {
+        if (colour[i] == -1)
+        {
+            int cnt0 = 0, cnt1 = 0;
 
-        queue<int> q;
-        q.push(i);
-        color[i] = 0;
-
-        int cnt0 = 1, cnt1 = 0;
-
-        while (!q.empty()) {
-            int u = q.front();
-            q.pop();
-
-            for (auto v : adj[u]) {
-                if (color[v] == -1) {
-                    color[v] = color[u] ^ 1;
-
-                    if (color[v] == 0) cnt0++;
-                    else cnt1++;
-
-                    q.push(v);
-                }
+            if (bfs(i, cnt0, cnt1))
+            {
+                ans += max(cnt0, cnt1);
             }
         }
-
-        answer += max(cnt0, cnt1);
     }
 
-    cout << answer << '\n';
+    cout << ans << "\n";
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        dosomethingbig();
+    }
+
+    return 0;
 }
